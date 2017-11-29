39c2dc7aaa7f0ca38f5c01156923217b660434bb
Katerina <t.ekaterina.y@gmail.com>
2017-11-29 11:32:10 +0300

    Commit 2 in develop
    Adding second commit to develop branch.

--- Commits in develop.js
+++ Commits in develop.js
@@ -1,6 +1,6 @@
 <script>
 
 document.write('Commit 1'+'<br>'+'Commit 2'+'<br>'+'Commit 2')
-document.write('Commit 1 in develop')
+document.write('Commit 1 in develop'+'<br>'+'Commit 2 in develop')
 
 </script>
